f479605b481d4e808d6fd6d162f19433aaec9f05
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-08-25 12:05:02 -0400

    added base html and api call urls

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// this is the 5-day forecast url
+var weatherUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+// this is the api url that gets lat and long of specific places
+var geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+// this is my api key. it goes at the end of each url
+var apiKey = "3e1d2d78e1cc6cb9267d3e61fa4244d6"
