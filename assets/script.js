0836149196e0e3d5fce26a8d2d029896648fae27
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-08-31 12:02:03 -0400

    fixed api urls

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,3 @@
-// this is the 5-day forecast url
-var weatherUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-// this is the api url that gets lat and long of specific places
-var geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
-// this is my api key. it goes at the end of each url
-var apiKey = "3e1d2d78e1cc6cb9267d3e61fa4244d6"
-
 var searchBar = document.querySelector("#searchBar");
 var searchButton = document.querySelector("#searchButton");
 var fiveDayForecast = document.querySelector("#fiveDayForecast");
@@ -60,7 +53,7 @@ function showHistory() {
 
 function getGeoUrl(place) {
     var city = place.toLowerCase();
-    var geoUrlPrefix = "http://api.openweathermap.org/geo/1.0/direct?q=";
+    var geoUrlPrefix = "https://api.openweathermap.org/geo/1.0/direct?q=";
     var geoUrlSuffix = "&limit=1&appid=3e1d2d78e1cc6cb9267d3e61fa4244d6";
 
     for (var i=0; i<city.length; i++){
